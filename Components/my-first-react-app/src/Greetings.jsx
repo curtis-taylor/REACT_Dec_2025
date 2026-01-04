93bd6a355b09c9efa0bbe67f6aedd7f548a58d48
curtis taylor <curtis.t@gmail.com>
2026-01-04 03:24:21 -0500

    tttt

--- Components/my-first-react-app/src/Greetings.jsx
+++ Components/my-first-react-app/src/Greetings.jsx
@@ -1,6 +1,9 @@
 function Greeting() {
   return <div> 
     <h3>TEST </h3>
-    <h1>&quot;I swear by my pretty floral bonnet, I will end you.&quot;</h1>
+    <button>press</button>
+    <h1>&quot;**I swear by my pretty floral bonnet, I will end you.&quot;</h1>
     </div>;
 }
+
+export default Greeting;
